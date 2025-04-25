5b82cd276a1afde9709683838f8cf9457624700c
CapVin17 <116640909+CapVin17@users.noreply.github.com>
2025-04-25 17:50:03 +0530

    done

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import { QRCodeCanvas } from "qrcode.react";
 import axios from "axios";
 

4503be5b7b4265072739368e49f0a5dc4f9493bf
CapVin17 <116640909+CapVin17@users.noreply.github.com>
2025-04-25 17:43:25 +0530

    changed uri

--- app/lib/dbConnect.js
+++ app/lib/dbConnect.js
@@ -12,7 +12,7 @@ async function dbConnect()
     }
 
     try {
-        const db = await mongoose.connect(process.env.MONGO_URI);
+        const db = await mongoose.connect(process.env.Mongo_Uri);
         connection.isConnected = db.connections[0].readyState;
         console.log("DB connected successfully");
     } catch (error) {
